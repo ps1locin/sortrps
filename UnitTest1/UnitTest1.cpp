f9b77ba2f36aba38ce63eaaa5291c5dbd5f9c4e6
ps1locin <maks.michaylovich20@mail.ru>
2026-02-23 19:37:57 +0300

    Добавьте файлы проекта.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+
+#include "merge_sort.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SortAppTests
+{
+    TEST_CLASS(MergeSortTests)
+    {
+    private:
+        static bool is_sorted(const std::vector<int>& a)
+        {
+            for (size_t i = 1; i < a.size(); ++i)
+                if (a[i - 1] > a[i]) return false;
+            return true;
+        }
+
+    public:
+        TEST_METHOD(ReverseArray)
+        {
+            std::vector<int> a{ 5,4,3,2,1 };
+            sorting::merge_sort(a);
+            Assert::IsTrue(is_sorted(a));
+        }
+
+        TEST_METHOD(EmptyArray)
+        {
+            std::vector<int> a;
+            sorting::merge_sort(a);
+            Assert::IsTrue(a.empty());
+        }
+
+        TEST_METHOD(Duplicates)
+        {
+            std::vector<int> a{ 3,1,3,0,2 };
+            sorting::merge_sort(a);
+            Assert::IsTrue(is_sorted(a));
+        }
+    };
+}
